1d6fff37cd0468c3c337e7feccf8c6af15cf7871
Max-le <max.09@outlook.com>
2018-01-18 21:45:47 +0100

    Add html code to displey duration of the video selected.

--- script.js
+++ script.js
@@ -5,12 +5,12 @@
 let api_key = "key=AIzaSyChnjvA4bfCdWECZuaYcw38sF2pWHcrhdM" ;
 //var base = "https://www.googleapis.com/youtube/v3/search?";
 var base = 'https://www.googleapis.com/youtube/v3/playlistItems?';
-var part = '&part=snippet';
+var part = '&part=snippet,contentDetails';
 //mot de recherche
 var q = '&q=bitcoin';
 //ID of playlist
 var id = '&playlistId=PLkR3RWLcKJUQas7CJWD1jQXpRmDbPRVE0'
-
+var duration ;
 //Création de l'URL
 var url = base + api_key + part +id;
 //next page token
@@ -34,7 +34,7 @@ $.get( url, function( data ) {
 	}
 });
 //Requete délcenchée par le button
-$("button").click(function(){
+$("#btn_submit").click(function(){
 
 	if (token != undefined) {
 //get next page with token
@@ -46,7 +46,7 @@ $.get( url, function( data ) {
 	console.log(data);
 	token = data.nextPageToken
 
-	console.log('next page token : '+ token);
+	console.log('next page token : '+ token);  
 	console.log('lenght of "item" array : '+ data.items.length)
 	for (i = 0; i<data.items.length; i++){
 		console.log(data.items[i].snippet.title);
@@ -62,3 +62,29 @@ $.get( url, function( data ) {
 }
 
 });
+
+//Get duration of a video 
+function getTime(id_video) {
+
+	console.log('in getTime() function !')
+base =  "https://www.googleapis.com/youtube/v3/videos?"
+id_video = "&id="+ id_video
+url = base+api_key+part + id_video
+$.get( url, function( data ) {
+console.log('http request for a video : '+url);
+duration = data.items[0].contentDetails.duration
+console.log("in function result : "+ duration);
+$("#duration").append("<br>"+duration);
+});
+}
+
+
+
+$("#btn_video").click(function(){
+	console.log("button video clicked !")
+	//TO DO : Solve undefined value returned from function
+	var x = getTime("8l8mF2xWv5c");
+	console.log('result from getTime() : ' + duration)
+});
+
+
