6e5016fb9ecd40fa20721fd744dc157dbb6846d1
Max-le <max.09@outlook.com>
2018-01-18 11:53:06 +0100

    Add explanatory comments.

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
+//La 1ère requete se fait lors du chargement de la page.
+// Elle obtient le token qui permettra de faire la requete qui suit ( en cliquant le button.)
+
+
+
+
+
 let api_key = "key=AIzaSyChnjvA4bfCdWECZuaYcw38sF2pWHcrhdM" ;
 var base = "https://www.googleapis.com/youtube/v3/search?";
 var part = '&part=snippet';
 //mot de recherche
 var q = '&q=norman';
 var maxresults= "&maxresults=20"
+
+//Création de l'URL
 var url = base + api_key + q + part ;
 var token;
+
+//1ère requête 
 $.get( url, function( data ) {
 	console.log('request launched with this url : '+ url);
 	$("p").append('request launched with this url : <a href='+url+'>'+ url+'</a>'+"<br>");
 	console.log(data);
+
+	//Obtenir token pour pouvoir charger la suite 
 	token = data.nextPageToken
 
 	console.log('next page token : '+ token);
@@ -19,7 +32,7 @@ $.get( url, function( data ) {
 		$("p").append(data.items[i].snippet.title + "<br>");
 	}
 });
-
+//Requete délcenchée par le button
 $("button").click(function(){
 //get next page with token
 url = base + api_key +"&pageToken="+token+ q + part ;
